7059b8fee5f2f0e66c63cfd2709b113c83af2252
vaishnavi <vaishnaviu2253@gmail.com>
2023-10-02 18:33:06 +0530

    product

--- src/Componet/Home.js
+++ src/Componet/Home.js
@@ -1,10 +1,8 @@
 import React from 'react'
-import  Tophead from './Tophead'
 import Founder from './Founder';
 import Cards from './Cards';
 import Counter from './Counter';
 import ControlledCarousel from './ControlledCarousel';
-import Swiper3D from './Swiper3D';
 function Home() {
   return (
     <div>
